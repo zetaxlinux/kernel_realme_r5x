78b923dde1d9c15f2dc1386989e89c7c79270398
zetaxlinux <142310052+zetaxlinux@users.noreply.github.com>
2024-01-24 05:04:55 +0800

    Remove some strings bash

--- compile.sh
+++ compile.sh
@@ -12,14 +12,7 @@ echo  $P
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-# Scrip option
-while (( ${#} )); do
-    case ${1} in
-        "-Z"|"--zip") ZIP=true ;;
-    esac
-    shift
-done
-[[ -z ${ZIP} ]] && { echo "${bold}LOADING-_-....${normal}"; }
+echo "${bold}LOADING-_-....${normal}"
 echo
 
 DEFCONFIG="vendor/R5X_defconfig"
@@ -28,19 +21,8 @@ export KBUILD_BUILD_HOST=Herobrine
 TC_DIR="/workspace/gitpod/clang-r498229b"
 export PATH="$TC_DIR/bin:$PATH"
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit
-fi
-
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-rm -rf out
-fi
-
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
-
 
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
 
