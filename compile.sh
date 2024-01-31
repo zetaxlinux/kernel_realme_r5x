de54d159756c6b74892901e3b9649191563f2875
zetaxlinux <142310052+zetaxlinux@users.noreply.github.com>
2024-01-31 23:51:41 +0800

    change colour compiler to cyan colour

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 make clean ; make mrproper
 #!/bin/bash
 
-echo -e "\033[93m==========================="
-echo -e "\033[93m= START COMPILING KERNEL  ="
-echo -e "\033[93m==========================="
+echo -e "\033[96m===========================\033[0m"
+echo -e "\033[96m= START COMPILING KERNEL  =\033[0m"
+echo -e "\033[96m===========================\033[0m"
 echo
 echo -e -n "\033[93m\033[104mPRESS ENTER TO CONTINUE\033[0m"
 read P
@@ -26,6 +26,6 @@ make O=out ARCH=arm64 $DEFCONFIG
 
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
 
-    echo -e "==========================="
-    echo -e "   COMPILE KERNEL COMPLETE "
-    echo -e "==========================="
+    echo -e "\033[96m===========================\033[0m"
+    echo -e "\033[96m   COMPILE KERNEL COMPLETE \033[0m"
+    echo -e "\033[96m===========================\033[om"
